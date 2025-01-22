47cbf6a2f7a119a2f48a6fca10793fbac06e89f7
harunobu0121 <u181980i@ecs.osaka-u.ac.jp>
2025-01-22 18:45:14 +0900

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const result = document.getElementById('result');
 const fortunes = ['大吉', '中吉', '小吉', '吉', '末吉', '凶', '大凶'];
 
 btn.addEventListener('click', () => {
-  const randomIndex = Math.floor(Math.random() * fortunes.length);
-  const fortune = fortunes[randomIndex];
-  result.textContent = fortune;
+ const randomIndex = Math.floor(Math.random() * fortunes.length);
+ const fortune = fortunes[randomIndex];
+ result.textContent = fortune;
 });
+
