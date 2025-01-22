141d2d809c879451ceb3800e3c35c0a26d86dae9
harunobu0121 <u181980i@ecs.osaka-u.ac.jp>
2025-01-22 18:44:38 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,20 @@
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open('static-assets').then((cache) => {
-      return cache.addAll([
-        '/',
-        'style.css',
-        'script.js'
-      ]);
-    })
-  );
+ event.waitUntil(
+   caches.open('static-assets').then((cache) => {
+     return cache.addAll([
+       '/',
+       'style.css',
+       'script.js'
+     ]);
+   })
+ );
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        return response || fetch(event.request);
-      })
-  );
+ event.respondWith(
+   caches.match(event.request)
+     .then((response) => {
+       return response || fetch(event.request);
+     })
+ );
 });
